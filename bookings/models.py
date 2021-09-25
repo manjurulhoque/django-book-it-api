5b042fc2212cecfd5baf8208381f27b972b9824e
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-09-25 10:45:38 +0600

    Room model created

---
+++ bookings/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price_per_night = models.FloatField()
+    address = models.CharField(max_length=255)
+    guest_capacity = models.IntegerField()
+    beds = models.IntegerField()
+    internet = models.BooleanField()
+    breakfast = models.BooleanField()
+    air_conditioned = models.BooleanField()
+    pets_allowed = models.BooleanField()
+    room_cleaning = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
