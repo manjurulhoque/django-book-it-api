b8e0624382cf7152cfc38170cdd976f24ad37f84
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-09-27 22:48:00 +0600

    Register API view

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -3,8 +3,21 @@ from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ("password", "user_permissions", "groups", "is_staff", "is_active", "is_superuser")
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ("username", "email", "password")
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        instance.set_password(password)
+        instance.save()
+        return instance
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,7 +1,19 @@
 from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
 
-from .serializers import UserSerializer
+from .serializers import UserCreateSerializer, UserSerializer
 
 
 class RegisterAPIView(generics.CreateAPIView):
-    serializer_class = UserSerializer
+    serializer_class = UserCreateSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        user_serializer = UserSerializer(serializer.instance)
+
+        return Response(user_serializer.data, status=status.HTTP_201_CREATED, headers=headers)
